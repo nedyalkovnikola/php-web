d6e698d53094e3465e455ca3c4acebb4d32f4101
Nikola Nedyalkov <nedyalkov.nikola@gmail.com>
2022-04-14 14:29:20 +0300

    updated

--- 05.SocialNetwork/login.php
+++ 05.SocialNetwork/login.php
@@ -1,4 +1,21 @@
 <?php
+use Service\User\UserService;
+
 require_once 'app.php';
+
+
+if(isset($_POST['login'])) {
+    $userService = new UserService($db, $encryptionService);
+    $username = $_POST['nickname'];
+    $password = $_POST['password'];
+
+    if (!$userService->login($username, $password)) {
+        throw new Exception("Password mismatch");
+    }
+
+    header("Location: profile.php");
+    exit;
+
+}
 
 $app->loadTemplate('login_view');
--- 05.SocialNetwork/profile.php
+++ 05.SocialNetwork/profile.php
@@ -1,1 +1,10 @@
-<?
+<?php
+require_once 'app.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit;
+    throw new Exception("This page is restricted to logged in users");
+}
+
+$app->loadTemplate('profile_view');
